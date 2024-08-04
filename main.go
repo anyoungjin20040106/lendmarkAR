dbde9c0b0127c2bdf9efa82c08b7c1832fc623e3
안영진 <anyoungjin0106@kbu.ac.kr>
2024-08-04 20:52:19 +0900

    create web

--- main.go
+++ main.go
@@ -1,34 +1,49 @@
 package main
 
 import (
-	"html/template"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type PageData struct {
+	Name string
+}
+
 func main() {
 	r := gin.Default()
-	temp := template.Must(template.ParseFiles("index.html"))
+	r.Static("/Build", "./Build")
+
+	// TemplateData 디렉토리 마운트
+	r.Static("/TemplateData", "./TemplateData")
+	// HTML 템플릿 파일 로드
+	r.LoadHTMLFiles("index.html")
+
 	// POST 요청 처리
-	r.POST("/", func(ctx *gin.Context) {
-		// 폼 데이터 수신
-		name := ctx.PostForm("name")
-		content := ctx.PostForm("content")
-		imgUrl := ctx.PostForm("img")
+	r.GET("/", func(ctx *gin.Context) {
 
 		// 익명 구조체를 사용하여 데이터를 저장
 		responseData := struct {
-			Name    string `json:"name"`
-			Content string `json:"content"`
-			ImgURL  string `json:"imgUrl"`
+			Name string `json:"name"`
 		}{
-			Name:    name,
-			Content: content,
-			ImgURL:  imgUrl,
+			Name: "수종사",
 		}
-		temp.Execute(ctx.Writer, responseData)
+		ctx.HTML(http.StatusOK, "index.html", responseData)
+	})
+	// POST 요청 처리
+	r.POST("/", func(ctx *gin.Context) {
+		// 예시 폼 데이터 수신
+		name := ctx.PostForm("name")
+
+		// 템플릿에 전달할 데이터 구조체
+		pageData := PageData{
+			Name: name,
+		}
+
+		// HTML 템플릿 렌더링
+		ctx.HTML(http.StatusOK, "index.html", pageData)
 	})
 
 	// 서버 실행
-	r.Run(":8080") // 기본 포트는 8080입니다.
+	r.Run(":1357")
 }
